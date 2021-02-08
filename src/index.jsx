47aa7459759e3b236dbb12eb4ea87cfaaae7551f
Elad Hayun <elad.hayun@gmail.com>
2021-02-08 14:53:45 +0200

    Move the store to its own file

--- src/index.jsx
+++ src/index.jsx
@@ -2,31 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App/App'
+import { getStore } from './store'
 import * as utils from './utils'
-import rootReducer from './reducers'
 import { routes } from './consts'
 import { createBrowserHistory } from 'history'
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
-import { createStore, applyMiddleware } from 'redux'
+import { ConnectedRouter } from 'connected-react-router'
 import { Provider } from 'react-redux'
 import { Route, Switch, Redirect } from 'react-router'
-import {
-  ConnectedRouter,
-  routerMiddleware as createRouterMiddleware,
-} from 'connected-react-router'
 
 const history = createBrowserHistory()
-const composeEnhancers = composeWithDevTools({})
-const routerMiddleware = createRouterMiddleware(history)
-
-export const store = createStore(
-  rootReducer(history),
-  composeEnhancers(applyMiddleware(routerMiddleware))
-)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={getStore(history)}>
       <ConnectedRouter history={history}>
         <Switch>
           <Route path={routes.ROOT} component={App} />
